0df96d902feac40f5f1c4c69e89e61202162634b
Ankan Ghosh <ankan005@gmail.com>
2018-10-10 11:24:10 +0530

    Update xtended_rolex.mk

--- xtended_rolex.mk
+++ xtended_rolex.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+$(call inherit-product, vendor/dot/config/common_full_phone.mk)
 
 # Inherit from rolex device
 $(call inherit-product, device/xiaomi/rolex/device.mk)
@@ -29,8 +29,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := rolex
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 4A
-PRODUCT_NAME := xtended_rolex
-XTENDED_BUILD_TYPE := OFFICIAL
+PRODUCT_NAME := dot_rolex
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
