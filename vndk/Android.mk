43aae26860937515da5d325aeb0ac4643cf8ae76
Ankan005 <Ankan005@gmail.com>
2018-11-02 06:44:45 +0000

    vndk-sp_grneration

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -14,6 +14,7 @@ VNDK_SP_LIBRARIES := \
     libc++ \
     libcompiler_rt \
     libcutils \
+    libdexfile \
     libft2 \
     libhardware \
     libhidlbase \
@@ -26,6 +27,8 @@ VNDK_SP_LIBRARIES := \
     libunwind \
     libutils \
     libz
+    libartbase \
+    libziparchive
 
 EXTRA_VENDOR_LIBRARIES := \
     android.hidl.base@1.0 \
@@ -34,6 +37,8 @@ EXTRA_VENDOR_LIBRARIES := \
     vendor.display.config@1.0 \
     vendor.qti.hardware.iop@1.0
 
+install_in_hw_dir := \
+   android.hidl.memory@1.0-impl
 
 #-------------------------------------------------------------------------------
 # VNDK Modules
@@ -44,7 +49,7 @@ define define-vndk-lib
 include $$(CLEAR_VARS)
 LOCAL_MODULE := $1.$2
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_PREBUILT_MODULE_FILE := $$(TARGET_OUT_INTERMEDIATE_LIBRARIES)/$1.so
+LOCAL_PREBUILT_MODULE_FILE := $$(call intermediates-dir-for,SHARED_LIBRARIES,$1,,,,)/$1.so
 LOCAL_STRIP_MODULE := false
 LOCAL_MULTILIB := first
 LOCAL_MODULE_TAGS := optional
@@ -59,7 +64,7 @@ ifneq ($$(TARGET_TRANSLATE_2ND_ARCH),true)
 include $$(CLEAR_VARS)
 LOCAL_MODULE := $1.$2
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_PREBUILT_MODULE_FILE := $$($$(TARGET_2ND_ARCH_VAR_PREFIX)TARGET_OUT_INTERMEDIATE_LIBRARIES)/$1.so
+LOCAL_PREBUILT_MODULE_FILE := $$(call intermediates-dir-for,SHARED_LIBRARIES,$1,,,$$(TARGET_2ND_ARCH_VAR_PREFIX),)/$1.so
 LOCAL_STRIP_MODULE := false
 LOCAL_MULTILIB := 32
 LOCAL_MODULE_TAGS := optional
@@ -79,6 +84,16 @@ $(foreach lib,$(VNDK_SP_EXT_LIBRARIES),\
 $(foreach lib,$(EXTRA_VENDOR_LIBRARIES),\
     $(eval $(call define-vndk-lib,$(lib),vndk-ext-gen,,true)))
 
+# Add VNDK-SP libs to the list if they are missing
+$(foreach lib,$(VNDK_SAMEPROCESS_LIBRARIES),\
+    $(if $(filter $(lib),$(VNDK_SP_LIBRARIES)),,\
+    $(eval VNDK_SP_LIBRARIES += $(lib))))
+
+# Remove libz from the VNDK-SP list (b/73296261)
+VNDK_SP_LIBRARIES := $(filter-out libz,$(VNDK_SP_LIBRARIES))
+
+$(foreach lib,$(VNDK_SP_LIBRARIES),\
+    $(eval $(call define-vndk-sp-lib,$(lib))))
 
 #-------------------------------------------------------------------------------
 # Phony Package
@@ -86,9 +101,11 @@ $(foreach lib,$(EXTRA_VENDOR_LIBRARIES),\
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := vndk-sp
+LOCAL_MODULE_OWNER := google
 LOCAL_MODULE_TAGS := optional
 LOCAL_REQUIRED_MODULES := \
     $(addsuffix .vndk-sp-gen,$(VNDK_SP_LIBRARIES)) \
     $(addsuffix .vndk-sp-ext-gen,$(VNDK_SP_EXT_LIBRARIES)) \
     $(addsuffix .vndk-ext-gen,$(EXTRA_VENDOR_LIBRARIES))
 include $(BUILD_PHONY_PACKAGE)
+
